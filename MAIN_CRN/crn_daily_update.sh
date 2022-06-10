4faf87e521f5e373df3391cde5948b067a019203
pcampbe8 <pcampbe8@hopper1.orc.gmu.edu>
2022-06-09 21:59:05 -0400

    Updated main CRN script for info...

--- MAIN_CRN/crn_daily_update.sh
+++ MAIN_CRN/crn_daily_update.sh
@@ -11,21 +11,24 @@ source ./setcase.csh
 source ./module_file
 
 #1.  Download and process daily CRN for SWAMP in respecitve year.
+echo "DOWNLOADING  CRN for SWAMP------------------------>"
 ./downftp
 echo "finished downftp"
 
 #2.  Download and process daily PRISM for SWAMP in respective year.
+echo "DOWNLOADING/PROCESSING PRISM for SWAMP------------------------>"
 cd ../PRISM
 ./runprism
 echo "finished runprism"
 
 #3.   Download daily ALEXI for SWAMP.
+echo "DOWNLOADING DAILY ALEXI DATA for SWAMP------------------------>"
 cd ../ALEXI
 ./runalexi
 echo "finished runalexi:"
 
 #4.  This calls the main SWAMP processing for daily and daily_station outputs.
-
+echo "DOWNLOADING GRIDDED VEGETAION DATA AND RUNNING SWAMP------------------------>"
 cd ../MAIN_CRN
 ./getvegsoilnc4swamp
 ./runscript
