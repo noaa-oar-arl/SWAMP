3354901323f8686dd461f6b63c63e120534ed207
zmoon <zmoon92@gmail.com>
2022-08-24 17:10:47 -0600

    notes

--- swamp.py
+++ swamp.py
@@ -15,6 +15,7 @@ lon = np.linspace(-125, -65, ny)
 
 # For now, use the coeffs that already exist
 # NOTE: slp is the one that gets used in the current original SWAMP
+# NOTE: this array has 39% 0 values and 17% -21.52 values, the rest positive but < 1
 slp_coeffs = np.loadtxt(ORIG / "PROCESS_DAILY/slp_weights.txt")
 int_coeffs = np.loadtxt(ORIG / "PROCESS_DAILY/int_weights.txt")
 assert slp_coeffs.shape == int_coeffs.shape == (ny, nx)
