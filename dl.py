933daeeb5e5821599392b581adf0904ec57648d7
zmoon <zmoon92@gmail.com>
2022-08-24 14:41:27 -0600

    Initial PRISM data grabbing and parsing

--- dl.py
+++ dl.py
@@ -7,8 +7,10 @@ import re
 import warnings
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 import requests
+import xarray as xr
 
 CACHE_DIR = Path(__file__).parent / "cache"
 
@@ -18,7 +20,9 @@ assert CACHE_DIR.is_dir()
 def get_crn(days, *, use_cache=True):
     """Get daily soil (and vegetation?) CRN data for `days`.
 
-    https://www.ncei.noaa.gov/access/crn/qcdatasets.html
+    Info: https://www.ncei.noaa.gov/access/crn/qcdatasets.html
+
+    Data: https://www.ncei.noaa.gov/pub/data/uscrn/products/daily01/
     """
     days = pd.DatetimeIndex(days)
 
@@ -40,7 +44,7 @@ def get_crn(days, *, use_cache=True):
     # e.g. `>2000/<`
     r = requests.get(f"{base_url}/")
     r.raise_for_status()
-    available_years = re.findall(r">([0-9]{4})/?<", r.text)
+    available_years: list[str] = re.findall(r">([0-9]{4})/?<", r.text)
 
     # Get files
     # TODO: parallelize using Dask or joblib
@@ -110,9 +114,132 @@ def get_crn(days, *, use_cache=True):
     return df
 
 
+def get_prism(days):
+    """Get PRISM precip data.
+    
+    Info: https://prism.oregonstate.edu/
+
+    Data: https://ftp.prism.oregonstate.edu/daily/ppt/
+    """
+    import zipfile
+
+    days = pd.DatetimeIndex(days)
+
+    base_url = "https://ftp.prism.oregonstate.edu/daily/ppt"
+
+    # Get available years from the main page
+    # e.g. `>2000/<`
+    r = requests.get(f"{base_url}/")
+    r.raise_for_status()
+    available_years: list[str] = re.findall(r">([0-9]{4})/?<", r.text)
+
+    dss_per_day = []
+    fns_year = {}
+    ymds = days.strftime(r"%Y%m%d").unique()
+    # TODO: cache the files (probably just the straight zip ones? or nc?)
+    for ymd in ymds:
+        year = ymd[:4]
+        if year not in available_years:
+            raise ValueError(f"year {year} not in detected available years {available_years}")
+
+        available_fns = fns_year.get(year, None)
+        if available_fns is None:
+            # Get filenames from the year page
+            # e.g. `>PRISM_ppt_stable_4kmD2_20200121_bil.zip<`
+            url = f"{base_url}/{year}/"
+            r = requests.get(url)
+            r.raise_for_status()
+            available_fns = re.findall(r">(PRISM_ppt_[a-zA-Z0-9_]*_bil\.zip)<", r.text)
+            if not available_fns:
+                warnings.warn(f"no files found for year {year} (url {url})")
+            fns_year[year] = available_fns
+
+        # Download zip archive
+        # https://prism.oregonstate.edu/documents/PRISM_downloads_FTP.pdf
+        # e.g. `https://ftp.prism.oregonstate.edu/daily/ppt/1996/PRISM_ppt_stable_4kmD2_19960101_bil.zip`
+
+        # "For monthly and daily data, the “stability” portion of the filename will change over time.
+        #  Datasets from 6-12 months ago are considered “provisional” because they are
+        #  still likely to change; you will probably want to re-download the files
+        #  once their names say “stable” (i.e., after 12 months have elapsed)."
+        stabilities = ["stable", "provisional", "early"]
+
+        # Look for stable first, etc.
+        for stab in stabilities:
+            fn = f"PRISM_ppt_{stab}_4kmD2_{ymd}_bil.zip"
+            if fn in available_fns:
+                break
+        else:
+            raise ValueError(f"file for {ymd} not found. Check {base_url}/{year}/")
+        if stab in stabilities[1:]:
+            warnings.warn(f"note 'stable' file for {ymd} not found, using {stab!r}")
+
+        # Download file
+        url = f"{base_url}/{year}/{fn}"
+        print(url)
+        r = requests.get(url)
+        r.raise_for_status()
+        zip_fp = CACHE_DIR / fn
+        with open(zip_fp, "wb") as f:
+            f.write(r.content)
+
+        # Get the BIL from the zip
+        with zipfile.ZipFile(zip_fp, 'r') as zf:
+            bil_fn = str(Path(fn).with_suffix(".bil"))
+            try:
+                data = zf.read(bil_fn)  # bytes
+            except KeyError:
+                print(f"{bil_fn!r} not found in archive. Archive namelist: {zf.namelist()}")
+                raise
+
+            # hdr_fn = str(Path(fn).with_suffix(".hdr"))
+
+        # Read the BIL into an array
+        # https://pymorton.wordpress.com/2016/02/26/plotting-prism-bil-arrays-without-using-gdal/
+        # TODO: the metadata we use here is in the HDR text file, could get from there
+        prism_ncols = 1405
+        prism_nrows = 621
+        prism_nodata = -9999
+        prism_ulxmap = -125.000000000000
+        prism_ulymap =   49.916666666667
+        prism_xdim   =    0.041666666667  # 2.5 arc minutes, ~ 4 km
+        prism_ydim   =    0.041666666667
+        prism_array = np.frombuffer(data, dtype=np.float32)  # note: `np.fromfile()` didn't work
+        prism_array = prism_array.reshape(prism_nrows, prism_ncols)
+
+        # Construct Dataset
+        arr = np.where(prism_array == prism_nodata, np.nan, prism_array)  # note: couldn't modify buffer in place
+        lon = prism_ulxmap + np.arange(prism_ncols) * prism_xdim
+        lat = prism_ulymap - np.arange(prism_nrows) * prism_ydim
+        ds = xr.Dataset(
+            data_vars={
+                "ppt": (("lat", "lon"), arr, {
+                    "long_name": "Precipitation",
+                    "units": "mm",
+                    "description": "Daily total precipitation (rain + melted snow)",
+                }),
+                "ppt_stability": ((), stab, {
+                    "long_name": "Stability",
+                    "description": "'stable', 'provisional', or 'early', mostly depending on when the data was released",
+                }),
+            },
+            coords={
+                "lat": (("lat",), lat),
+                "lon": (("lon",), lon),
+            }
+        )
+        dss_per_day.append(ds)
+
+    # Combined Dataset
+    ds = xr.concat(dss_per_day, dim="time")
+    ds["time"] = (("time",), pd.to_datetime(ymds))
+
+    return ds
+
 if __name__ == "__main__":
     days = pd.date_range("2020/08/01", periods=2, freq="D")
     print(days)
 
-    df = get_crn(days)
+    # df = get_crn(days)
 
+    ds = get_prism(days)
