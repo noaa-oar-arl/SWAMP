8f7d93c71677310d1b56fb9892f9e128e264db94
zmoon <zmoon92@gmail.com>
2022-08-24 17:06:30 -0600

    Load weights
    Following orig/PROCESS_DAILY/Soil_moisture_model_grid_fine_rel.ncl
    for grid info and such

---
+++ swamp.py
@@ -0,0 +1,20 @@
+"""
+SWAMP
+"""
+from pathlib import Path
+
+import numpy as np
+
+HERE = Path(__file__).parent
+ORIG = HERE / "orig"
+
+# Define output grid
+ny, nx = (720, 1150)
+lat = np.linspace(25, 50, nx)
+lon = np.linspace(-125, -65, ny)
+
+# For now, use the coeffs that already exist
+# NOTE: slp is the one that gets used in the current original SWAMP
+slp_coeffs = np.loadtxt(ORIG / "PROCESS_DAILY/slp_weights.txt")
+int_coeffs = np.loadtxt(ORIG / "PROCESS_DAILY/int_weights.txt")
+assert slp_coeffs.shape == int_coeffs.shape == (ny, nx)
