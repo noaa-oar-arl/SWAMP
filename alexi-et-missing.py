b8dae5d8568c9f71d1b237a6b0d5b22d1e95f0ad
zmoon <zmoon92@gmail.com>
2023-03-13 13:43:13 -0600

    Parallel ALEXI ET file check

--- alexi-et-missing.py
+++ alexi-et-missing.py
@@ -1,7 +1,11 @@
 """
 Print list of dates in current year where ALEXI ET file is missing
 """
+from __future__ import annotations
+
 import re
+from datetime import date
+from multiprocessing import Pool
 
 import pandas as pd
 import requests
@@ -20,14 +24,30 @@ if not available_yjs:
         f"search of {base_url}/ detected no available dates for ALEXI ET",
     )
 
-# TODO: parallel
-for yj in available_yjs[-3:]:
+
+def check(yj: str) -> tuple[bool, date, str]:
     fn = f"ALEXI_ET_4KM_CONUS_V01_{yj}.dat"
     url = f"{base_url}/{yj}/{fn}"
-    print(url)
+    ts = pd.to_datetime(yj, format=r"%Y%j")
+    date_ = ts.date()
     r = requests.head(url)
+    ok = True
     if r.status_code == 404:
-        ts = pd.to_datetime(yj, format=r"%Y%j")
-        print(yj, str(ts.date()))
+        ok = False
     else:
         r.raise_for_status()
+    return ok, date_, url
+
+
+if __name__ == "__main__":
+    with Pool() as p:
+        res = p.map(check, available_yjs)
+
+    res.sort(key=lambda tup: tup[1])
+
+    n = 0
+    for ok, date_, url in res:
+        if not ok:
+            print(date_, url)
+            n += 1
+    print(f"{n}/{len(res)} missing ALEXI ET file")
